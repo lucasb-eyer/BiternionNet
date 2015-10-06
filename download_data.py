84f4626a623283c3c4d98d9be0ccd69fe837f772
lucasb-eyer <lucasb.eyer.be@gmail.com>
2015-10-06 12:36:23 +0200

    Update download URL and add more output to downloader.

--- download_data.py
+++ download_data.py
@@ -12,22 +12,25 @@ def here(f):
     return os.path.join(os.path.dirname(os.path.abspath(me)), f)
 
 
-def download_extract(url, into):
-    fname = download(url, into)
+def download_extract(urlbase, name, into):
+    print("Downloading " + name)
+    fname = download(os.path.join(urlbase, name), into)
     print("Extracting...")
     with tarfile.open(fname) as f:
         f.extractall(path=into)
 
 
 if __name__ == '__main__':
-    baseurl = 'https://omnomnom.vision.rwth-aachen.de/data/tosato/'
+    baseurl = 'https://omnomnom.vision.rwth-aachen.de/data/BiternionNets/'
     datadir = here('data')
 
     # First, download the Tosato datasets.
-    download_extract(baseurl + 'CAVIARShoppingCenterFullOccl.tar.bz2', into=datadir)
-    download_extract(baseurl + 'CAVIARShoppingCenterFull.tar.bz2', into=datadir)
-    download_extract(baseurl + 'HIIT6HeadPose.tar.bz2', into=datadir)
-    download_extract(baseurl + 'HOC.tar.bz2', into=datadir)
-    download_extract(baseurl + 'HOCoffee.tar.bz2', into=datadir)
-    download_extract(baseurl + 'IHDPHeadPose.tar.bz2', into=datadir)
-    download_extract(baseurl + 'QMULPoseHeads.tar.bz2', into=datadir)
+    download_extract(baseurl, 'CAVIARShoppingCenterFullOccl.tar.bz2', into=datadir)
+    download_extract(baseurl, 'CAVIARShoppingCenterFull.tar.bz2', into=datadir)
+    download_extract(baseurl, 'HIIT6HeadPose.tar.bz2', into=datadir)
+    download_extract(baseurl, 'HOC.tar.bz2', into=datadir)
+    download_extract(baseurl, 'HOCoffee.tar.bz2', into=datadir)
+    download_extract(baseurl, 'IHDPHeadPose.tar.bz2', into=datadir)
+    download_extract(baseurl, 'QMULPoseHeads.tar.bz2', into=datadir)
+
+    print("Done.")
